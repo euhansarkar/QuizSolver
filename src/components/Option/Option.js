b191b1da25ab0e9338b9c4ec294752b5335bad78
euhansarkar <euhansarkar@gmail.com>
2022-10-11 23:33:00 +0600

    alert added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
+const Option = ({ option, id, ind, handleCorrect }) => {
 
-const Option = ({ option, id, ind }) => {
+ 
   return (
-    <div className=" py-3 px-4 border border-blue-600 gap-3 m-4 rounded-xl">
-      <label className="flex" htmlFor={`${ind}-${id}`}>
+    <div className={`py-3 px-4 border border-blue-600 gap-1 m-4 rounded-xl`}>
+      <label className="flex items-center" onClick={() => handleCorrect(option)} htmlFor={`${ind}-${id}`}>
         <input className="mr-4" type="radio" name="rad" id={`${ind}-${id}`} />
         <h2>{option}</h2>
       </label>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,6 +6,11 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 const Quiz = ({ quiz, ind }) => {
   const [eye, setEye] = useState(false);
   const { id, options, question, correctAnswer } = quiz;
+
+  const handleCorrect = opt => {
+    return opt === correctAnswer ? alert(`this is correct ans`) : alert(`this is false ans`);
+  }
+
   return (
     <div className=" shadow-2xl relative w-4/5 m-auto px-40 my-12">
       <div onClick={() => setEye(!eye)} className="absolute right-24 top-3">
@@ -21,7 +26,7 @@ const Quiz = ({ quiz, ind }) => {
       </h2>
       <div className="grid md:grid-cols-2 pb-5">
         {options.map((option, ind) => (
-          <Option key={ind} id={id} ind={ind} option={option}></Option>
+          <Option key={ind} id={id} ind={ind} option={option} handleCorrect={handleCorrect}></Option>
         ))}
       </div>
       <div className="pb-10 text-blue-600">
